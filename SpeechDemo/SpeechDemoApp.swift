0f2442d6c7b3000998a1c9196a0129473478f982
cagrigider <cagrigider@gmail.com>
2023-11-07 11:20:37 +0300

    Initial Commit

---
+++ SpeechDemo/SpeechDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeechDemoApp.swift
+//  SpeechDemo
+//
+//  Created by Gider, Cagri on 7.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct SpeechDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
